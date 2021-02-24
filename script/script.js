9d573a096ce561b30356e1fba7a2f922046cd78e
Victor Voicu <victor_voicu61@yahoo.com>
2021-02-24 12:12:50 +0100

    collision cube

--- script/script.js
+++ script/script.js
@@ -3,11 +3,11 @@
 // Target config
 const aim = document.querySelector('#cube');
 const playground = document.querySelector('#playground');
-
 aim.addEventListener('click', function (e) {
+    aim.style.backgroundColor = 'red';
     console.log('you won');
-    alert('The mouse won');
     e.stopImmediatePropagation();
+    aim.style.backgroundColor = 'red';
 })
 
 playground.addEventListener('click', function (e) {
@@ -28,16 +28,29 @@ document.addEventListener('keydown', function (e) {
         posLeft++;
         playerKeyboard.style.left = posLeft + '%';
     };
+    if (posLeft > 93.7) {
+        posLeft = 0;
+    }
     if (e.code == "ArrowLeft") {
         posLeft--;
         playerKeyboard.style.left = posLeft + '%';
     };
+    if (posLeft < 0) {
+        posLeft = 93;
+    }
     if (e.code == "ArrowDown") {
         posTop++;
         playerKeyboard.style.top = posTop + '%';
     };
+    if (posTop > 86) {
+        posTop = 0;
+    }
     if (e.code == "ArrowUp") {
         posTop--;
         playerKeyboard.style.top = posTop + '%';
     };
+    if (posTop < 0) {
+        posTop = 86;
+    }
 })
+
